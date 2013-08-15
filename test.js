fe8187621c2f5455f300cacac36b9b6d5a524db2
Conrad Zimmerman <me@conradz.com>
2013-08-15 11:04:42 -0400

    Use <pre> element for displaying console output

--- test.js
+++ test.js
@@ -39,7 +39,7 @@ test('serve home page', function(t) {
             t.notOk(err);
             t.equal(r.statusCode, 200);
             t.ok(/^<!DOCTYPE html>/.test(body));
-            t.ok(/<div id="output"/.test(body));
+            t.ok(/<pre id="output"/.test(body));
 
             server.close(function() {
                 t.end();
@@ -55,7 +55,7 @@ test('skip output when noConsole is true', function(t) {
         request(url, function(err, r, body) {
             t.notOk(err);
             t.equal(r.statusCode, 200);
-            t.notOk(/<div id="output"/.test(body));
+            t.notOk(/<pre id="output"/.test(body));
 
             server.close(function() {
                 t.end();
